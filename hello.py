c8bdaac9e20d7fff89554444974bf1ddbbc8e10f
Priscilla <priscilla.deng@mail.utoronto.ca>
2022-09-25 02:38:49 -0400

    Add flash popups for Activity 4

--- hello.py
+++ hello.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, EmailField
-from wtforms.validators import DataRequired, Email, ValidationError
+from wtforms.validators import DataRequired, Email
 
 from datetime import datetime
 
@@ -14,25 +14,29 @@ bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 class NameForm(FlaskForm):
-
-    def validate_uoft_email(form, field):
-        if 'utoronto' not in field.data:
-            raise ValidationError('Please enter a utoronto email')
-
     name = StringField('What is your name?', validators=[DataRequired()])
-    email = EmailField('What is your UofT email?', validators=[DataRequired(), Email(), validate_uoft_email])
+    email = EmailField('What is your UofT email?', validators=[DataRequired(), Email()])
     submit = SubmitField('Submit')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    name, email = None, None
+    UofTEmail = False
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-        email = form.email.data
-        form.email.data = ''
-    return render_template('index.html', form=form, name=name, email=email)
+        old_name = session.get('name')
+        old_email = session.get('email')
+
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
+        session['email'] = form.email.data
+        return redirect(url_for('index'))
+    
+    if 'utoronto' in session['email']:
+        UofTEmail = True
+    return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'), UofTEmail=UofTEmail)
 
 @app.route('/user/<name>')
 def user(name):
